11846cb23e2c23febfc58da18279474d2b98aca3
Pratik Sonthaliya <pratiksonthaliya@gmail.com>
2024-04-19 19:21:31 +0530

    initial commit

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,11 @@
+import { EmptyOrg } from "./_components/empty-org";
+
+export default function DashboardPage() {
+  return(
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+      <div>
+        <EmptyOrg />
+      </div>
+    </div>
+  ) 
+}
